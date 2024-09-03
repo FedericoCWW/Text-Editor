1e8657aee4e9da10c0c0c9e14f2fef440c85e3a7
Federico <133919977+FedericoCWW@users.noreply.github.com>
2024-09-03 13:38:56 -0300

    añadi una funcion para cambiar la fuente (de forma no dinamica)

--- text-editor.py
+++ text-editor.py
@@ -13,18 +13,18 @@ class Editor_Texto:
     font_list = ['Arial', 'Times New Roman', 'Impact', 'Georgia', 'Comfortaa']
     def __init__(self, root):
         self.root = root
-    def nuevoFile():
+    def nuevoFile(self):
         global filename
         filename = 'untitled'
         text.delete(0.0, END)
 
-    def guardaFile():
+    def guardaFile(self):
         global filename
         t = text.get(0.0, END)
         f = open(filename, 'w')
         f.write(t)
         f.close()
-    def guardarFIlecomo():
+    def guardarFIlecomo(self):
         f = asksaveasfile(defaultextension= '.txt')
         t = text.get("1.0", "end-1c")
         try:
@@ -32,7 +32,7 @@ class Editor_Texto:
         except:
             messagebox.showerror(title="Error!", message="No se pudo guardar el archivo de texto!")
 
-    def abrirFile():
+    def abrirFile(self):
         global filename
         file = askopenfile(parent=root,title='Select a File')
         filename = file.name
@@ -42,7 +42,7 @@ class Editor_Texto:
         file.close()
 
     #write a function that lets me search for a word in the file
-    def BuscarPalabra():
+    def BuscarPalabra(self):
         word = askstring(title="buscar", prompt="Ingrese texto")
         text.tag_remove("match", 1.0, END)
         matches = 0
@@ -58,8 +58,18 @@ class Editor_Texto:
                 start_pos = end_pos
                 text.tag_config('match', foreground='black', background='white')
     
-    def CambiarFuente():
-        pass
+    def CambiarFuente(self):
+        print(self.font_list)
+        top = Toplevel() # use Toplevel() instead of Tk()
+        top.geometry('300x100')
+        Label(top, text='Select your value', font=20).pack(pady=10)
+        variable = StringVar()
+        combo = ttk.Combobox(top, textvariable=variable, values=self.font_list)
+        combo.bind('<<ComboboxSelected>>', lambda _: top.destroy())
+        combo.pack()
+        top.grab_set()
+        top.wait_window(top)  # wait for itself destroyed, so like a modal dialog
+        text.configure(font=variable.get())
     
 if __name__ == "__main__":
     root = Tk()
@@ -81,12 +91,12 @@ if __name__ == "__main__":
 
     filemenu = Menu(menubar, tearoff=False)
     menubar.add_cascade(label="Archivo", menu=filemenu)
-    menubar.add_command(label="Buscar", command=Editor_Texto.BuscarPalabra)
-    menubar.add_command(label="Cambiar fuente", command=Editor_Texto.CambiarFuente)
-    filemenu.add_command(label="Nuevo", command=Editor_Texto.nuevoFile)
-    filemenu.add_command(label="Abrir", command=Editor_Texto.abrirFile)
-    filemenu.add_command(label="Guardar", command=Editor_Texto.guardaFile)
-    filemenu.add_command(label="Guardar Como", command=Editor_Texto.guardarFIlecomo)
+    menubar.add_command(label="Buscar", command=editor.BuscarPalabra)
+    menubar.add_command(label="Cambiar fuente", command=editor.CambiarFuente)
+    filemenu.add_command(label="Nuevo", command=editor.nuevoFile)
+    filemenu.add_command(label="Abrir", command=editor.abrirFile)
+    filemenu.add_command(label="Guardar", command=editor.guardaFile)
+    filemenu.add_command(label="Guardar Como", command=editor.guardarFIlecomo)
     filemenu.add_separator()
     filemenu.add_command(label="Salir", command=root.quit)
 
